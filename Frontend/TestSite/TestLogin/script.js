754ead4aa468de279baf24f806c1f1461f150af9
Winston Ung <ungw002@gmail.com>
2026-02-08 22:03:05 +0000

    #6 created dummy login page

---
+++ Frontend/TestSite/TestLogin/script.js
@@ -0,0 +1,21 @@
+// document.getElementById("login").addEventListener("click", loginFunc);
+
+
+// function loginFunc() {
+//     const username = document.getElementById("username");
+//     const password = document.getElementById("password");
+
+//     fetch("https://jsonplaceholder.typicode.com/todos", {
+//         method: "POST",
+//         body: JSON.stringify({
+//             userId: 1,
+//             title: "Fix my bugs",
+//             completed: false
+//     }),
+//         headers: {
+//             "Content-Type": "application/json; charset=UTF-8"
+//     }
+//     })
+//     then(response => response.json())
+//     then(json => console.log(json))
+// }
