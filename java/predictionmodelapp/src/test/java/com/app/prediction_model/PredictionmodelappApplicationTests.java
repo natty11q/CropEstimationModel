e4d50c7e76096ec6a959050047336ea1c76a6bac
natty11q <nath.fripm.san@gmail.com>
2026-02-08 21:27:18 +0000

    #4 refactor java setup slightly

---
+++ java/predictionmodelapp/src/test/java/com/app/prediction_model/PredictionmodelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.prediction_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionmodelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
