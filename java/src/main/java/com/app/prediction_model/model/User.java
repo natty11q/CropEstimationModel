0f588bdcde42b0e38a504a978799ce225c34d7ab
natty11q <nath.fripm.san@gmail.com>
2026-02-09 00:39:39 +0000

    move java content

---
+++ java/src/main/java/com/app/prediction_model/model/User.java
@@ -0,0 +1,125 @@
+package com.app.prediction_model.model;
+
+
+public class User {
+
+    
+    private long id;
+    
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+
+
+    protected User() {}
+
+      /**
+   * Constructs a new user object.
+   *
+   * @param id the unique ID of the user
+   * @param firstName the first name of the user
+   * @param lastName the last name of the user
+   * @param username the username of the user
+   * @param email the email address of the user
+   */
+    public User(Long id, String firstName, String lastName, String username, String email)
+    {
+
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+     /**
+     * Gets the user's first name.
+     *
+     * @return the user's first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the user's first name.
+     *
+     * @param newFirstName the new firstname
+     */
+    public void setFirstName(String newFirstName) {
+        firstName = newFirstName;
+    }
+
+    /**
+     * Gets the user's last name.
+     *
+     * @return the user's last name
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    /**
+     * Sets the user's last name.
+     *
+     * @param newLastName the new lastname
+     */
+    public void setLastName(String newLastName) {
+        lastName = newLastName;
+    }
+
+      /**
+     * Gets the user's full name.
+     *
+     * @return the full name in the format "First Last"
+     */
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+
+
+    /**
+     * Gets the user's username.
+     *
+     * @return the user's username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the user's username.
+     *
+     * @param newUserName the user's new username
+     */
+    public void setUsername(String newUserName) {
+        username = newUserName;
+    }
+
+
+    /**
+     * Gets the user's email address.
+     *
+     * @return the email address
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the user's email address.
+     *
+     * @param newEmail the new email address
+     */
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+}
